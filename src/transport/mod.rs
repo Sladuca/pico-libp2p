ca717e2224a0ffc71ef4cc4db09b9f07726dc70f
Sebastien La Duca <sladuca777@gmail.com>
2020-12-10 21:39:14 -0500

    using channels is slightly better???

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -3,22 +3,20 @@ use crate::peer::PeerID;
 use crate::stream::{LibP2PStream, Mux};
 
 use async_trait::async_trait;
-use futures::stream::BoxStream;
 use parity_multiaddr::Multiaddr;
 use std::io::{Read, Write};
-use tokio::io::{AsyncRead, AsyncWrite, Result as IoResult};
-
-pub mod tcp;
-pub mod tls;
-pub mod udp;
+use tokio::io::{AsyncRead, AsyncWrite, Result};
+use tokio::sync::{mpsc::Receiver, mpsc::Sender, oneshot::Receiver};
 
 /// A basic, not-necessarily-secure unequiped with a stream multiplexer. That is, a basic connection in libp2p terminology
+#[derive(Debug)]
 pub struct BasicConnection<Info, Channel: AsyncRead + AsyncWrite> {
     info: Info,
     channel: Channel,
 }
 
 /// A secure conn equipped with a stream multiplexer. That is, a CapableConn in libp2p terminology
+#[derive(Debug)]
 pub struct Connection<M: Mux, Info, Channel: AsyncRead + AsyncWrite> {
     conn: BasicConnection<Info, Channel>, // inner BasicConn
     local_peer_id: PeerID,                // this side's PeerID
@@ -29,38 +27,39 @@ pub struct Connection<M: Mux, Info, Channel: AsyncRead + AsyncWrite> {
 }
 
 /// Establishes connections, returning [BasicConn](struct.BasicConn.html)s
-#[async_trait]
 pub trait BasicTransport {
     type Channel: AsyncRead + AsyncWrite;
     type ConnInfo;
 
-    async fn listen<'a>(
+    fn listen(
         &mut self,
         addr: Multiaddr,
-    ) -> IoResult<BoxStream<'a, IoResult<BasicConnection<Self::ConnInfo, Self::Channel>>>>;
-    async fn dial(
+    ) -> oneshot::Receiver<
+        Result<mpsc::Receiver<Result<BasicConnection<Self::ConnInfo, Self::Channel>>>>,
+    >;
+    fn dial(
         &mut self,
         addr: Multiaddr,
-    ) -> IoResult<BasicConnection<Self::ConnInfo, Self::Channel>>;
+    ) -> oneshot::Receiver<Result<BasicConnection<Self::ConnInfo, Self::Channel>>>;
 }
 
 /// Establishes connections, returning [Connection](struct.Connection.html)s
-#[async_trait]
 pub trait Transport<M: Mux> {
     type Channel: AsyncRead + AsyncWrite;
     type ConnInfo;
 
-    async fn listen<'a>(
+    fn listen<'a>(
         &mut self,
         addr: Multiaddr,
-    ) -> IoResult<BoxStream<'a, IoResult<Connection<M, Self::ConnInfo, Self::Channel>>>>;
-    async fn dial(
+    ) -> oneshot::Receiver<
+        Result<mpsc::Receiver<Result<Connection<M, Self::ConnInfo, Self::Channel>>>>,
+    >;
+    fn dial(
         &mut self,
         addr: Multiaddr,
-    ) -> IoResult<Connection<M, Self::ConnInfo, Self::Channel>>;
+    ) -> oneshot::Receiver<Result<Connection<M, Self::ConnInfo, Self::Channel>>>;
 }
 
-#[async_trait]
 pub trait Upgrade<M: Mux>: BasicTransport {
     type T: Transport<
         M,
